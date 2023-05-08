741537ade534abdbf55f651d31411238164653b6
junhyeokLee <junhyeoklee616@gmail.com>
2023-05-08 12:46:13 +0900

    add dagger hilt & coroutines library

--- app/src/main/java/com/junhyeoklee/googlechart/ui/MainActivity.kt
+++ app/src/main/java/com/junhyeoklee/googlechart/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.junhyeoklee.googlechart
+package com.junhyeoklee.googlechart.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.junhyeoklee.googlechart.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
